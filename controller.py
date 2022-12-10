1fb6c6bc6a14accbfc40d03de7a55cb32c84bdc6
xInsomnian <98590267+xInsomnian@users.noreply.github.com>
2022-12-09 20:16:20 -0600

    uploading all documents

--- controller.py
+++ controller.py
@@ -29,7 +29,10 @@ class Controller(QMainWindow, Ui_MainWindow):
         self.__channel = Controller.MIN_CHANNEL
         self.volume_slider.setValue(self.__volume)
 
-    def tv_screen(self):
+    def tv_screen(self) -> None:
+        '''
+        This is responsible for the tv screen and what is displayed
+        '''
         if not self.__status:
             self.tv_output.clear()
         else:
@@ -42,9 +45,10 @@ class Controller(QMainWindow, Ui_MainWindow):
             elif self.__channel == 3:
                 self.tv_output.setPixmap(QtGui.QPixmap('NETFLIX.png'))
 
-
-
-    def power(self):
+    def power(self) -> None:
+        '''
+        this is in charge of powering on and off
+         '''
         if not self.__status:
             self.__status = True
         else:
@@ -52,17 +56,27 @@ class Controller(QMainWindow, Ui_MainWindow):
 
         Controller.tv_screen(self)
 
-    def volume_up(self):
+    def volume_up(self) -> None:
+        '''
+        this is in charge of the volume up and also displays with the bar on the side of the gui
+        '''
         if self.__status and not self.__muted and self.__volume < Controller.MAX_VOLUME:
             self.__volume += 1
             self.volume_slider.setValue(self.__volume)
 
-    def volume_down(self):
+    def volume_down(self) -> None:
+        '''
+        this is in charge of the volume down and also displays with the bar on the side of the gui
+        '''
+
         if self.__status and not self.__muted and self.__volume > Controller.MIN_VOLUME:
             self.__volume -= 1
             self.volume_slider.setValue(self.__volume)
 
-    def mute(self):
+    def mute(self) -> None:
+        '''
+        this mutes the tv which also disables the volume until it is unmuted
+        '''
         if self.__status:
             if not self.__muted:
                 self.__muted = True
@@ -75,14 +89,23 @@ class Controller(QMainWindow, Ui_MainWindow):
                 self.volume_up_button.setStyleSheet("color: rgb(255, 255, 255);")
                 self.label.setStyleSheet("color: rgb(255, 255, 255);")
 
-    def channel_up(self):
+    def channel_up(self) -> None:
+        '''
+        this is in charge of the channel up and making sure it only works when the tv is on
+        it also controlls the tv and what is on their
+        '''
         if self.__status:
             if self.__channel == Controller.MAX_CHANNEL:
                 self.__channel = Controller.MIN_CHANNEL
             else:
                 self.__channel += 1
         Controller.tv_screen(self)
-    def channel_down(self):
+
+    def channel_down(self) -> None:
+        '''
+        this is in charge of the channel down and making sure it only works when the tv is on
+        it also controlls the tv and what is on their
+        '''
         if self.__status:
             if self.__channel == Controller.MIN_CHANNEL:
                 self.__channel = Controller.MAX_CHANNEL
@@ -90,7 +113,11 @@ class Controller(QMainWindow, Ui_MainWindow):
                 self.__channel -= 1
         Controller.tv_screen(self)
 
-    def netflix(self):
+    def netflix(self) -> None:
+        '''
+        This function like on many new remotes has a netflix button so it jumps to the channel
+        where netflix is at while the tv is on
+        '''
         if self.__status:
             self.__channel = Controller.MAX_CHANNEL
         Controller.tv_screen(self)
